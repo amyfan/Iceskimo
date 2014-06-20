aad263e2f0bd4d467708cc7666ba3bce2dd69e96
Amy Fan <amyfan@gmail.com>
2014-06-19 17:07:01 -0700

    initial commit with rough broken layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iceskimo::Application.config.secret_key_base = '3c12e607219e1f8a6b06457be229116b20c0bb17214381fd74d38239c02b7671a7ebf3a791ffcaeb3f6f1478bd339ee94becd0530c8d62c7aba9eaaaf94f6641'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Iceskimo::Application.config.session_store :cookie_store, key: '_Iceskimo_session'
